e1965595656d996bec393a4dc7c0f4d31f6f330d
Anusha N <anushaashwini2005@gmail.com>
2026-05-13 20:28:09 +0530

    Add files via upload

---
+++ MODULES/SHAPES-TURTLE.py
@@ -0,0 +1,53 @@
+# SQUARE
+import turtle
+t = turtle.Turtle()
+
+for i in range(4):
+    t.forward(100)
+    t.right(90)
+
+turtle.done()
+'''
+# STAR
+import turtle
+
+t = turtle.Turtle()
+
+for i in range(5):
+    t.forward(150)
+    t.right(144)
+
+turtle.done()
+
+# CIRCLE
+import turtle
+
+t = turtle.Turtle()
+
+t.circle(100)
+
+turtle.done()
+
+# FLOWER DESIGN
+import turtle
+
+t = turtle.Turtle()
+t.speed(0)
+
+for i in range(36):
+    t.circle(100)
+    t.right(10)
+
+turtle.done()
+
+# HEXAGON
+import turtle
+
+t = turtle.Turtle()
+
+for i in range(6):
+    t.forward(100)
+    t.right(60)
+
+turtle.done()
+'''
